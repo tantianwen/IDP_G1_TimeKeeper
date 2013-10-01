efcc828a0fc3508ee312985976f2273dce7cdaae
James-Hong <huimin.hong.2011@sis.smu.edu.sg>
2013-10-01 11:25:13 +0800

    Committing again

--- src/com/example/istime/LoginActivity.java
+++ src/com/example/istime/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.istime;
 
+import com.example.istime_head.ProfileActivity;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
